546d9099f8c9c29de73968a782519100e084c1ed
Behrouz <bkhezry@gmail.com>
2019-09-01 16:13:34 +0430

    Create Constants class for handle constant values. - Add base url of API

---
+++ app/src/main/java/com/github/bkhezry/earthquake/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.github.bkhezry.earthquake.util
+
+class Constants {
+    companion object {
+        const val BASE_URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/"
+    }
+
+}
