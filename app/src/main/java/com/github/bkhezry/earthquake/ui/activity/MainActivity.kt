e5491bbc9f34c3cf39a0426214512c95bd3d832a
Behrouz <bkhezry@gmail.com>
2019-09-01 16:15:41 +0430

    Add request earthquake of hour in MainActivity - Create api service instance for get hourly earthquake. - Handle response or error of request.

--- app/src/main/java/com/github/bkhezry/earthquake/ui/activity/MainActivity.kt
+++ app/src/main/java/com/github/bkhezry/earthquake/ui/activity/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.github.bkhezry.earthquake.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.github.bkhezry.earthquake.R
-
+import com.github.bkhezry.earthquake.model.EarthquakeHourResponse
+import com.github.bkhezry.earthquake.service.ApiService
+import com.github.bkhezry.earthquake.util.ApiClient
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
-
+    private lateinit var apiService: ApiService
+    private val disposable = CompositeDisposable()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,17 +30,9 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        apiService = ApiClient.getClient()!!.create(ApiService::class.java)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
@@ -40,5 +40,29 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        getHourlyEarthquake()
+    }
+
+    private fun getHourlyEarthquake() {
+        val subscribe = apiService.getHourlyEarthquake()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(this::handleHourlyResponse, this::handleHourlyError)
+        disposable.add(subscribe)
+
+    }
+
+    private fun handleHourlyResponse(earthquakeHourResponse: EarthquakeHourResponse) {
+        Log.d("response:", earthquakeHourResponse.features.size.toString())
+    }
+
+    private fun handleHourlyError(error: Throwable) {
+        Log.d("message:", error.localizedMessage)
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        disposable.dispose()
     }
 }
