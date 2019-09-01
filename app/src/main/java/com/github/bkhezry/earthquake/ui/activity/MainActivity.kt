f5a95e70d5d08117e3c132342ba058a9f7d0515a
Behrouz <bkhezry@gmail.com>
2019-09-02 04:17:38 +0430

    set margin to bound fab.

--- app/src/main/java/com/github/bkhezry/earthquake/ui/activity/MainActivity.kt
+++ app/src/main/java/com/github/bkhezry/earthquake/ui/activity/MainActivity.kt
@@ -3,6 +3,7 @@ package com.github.bkhezry.earthquake.ui.activity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.coordinatorlayout.widget.CoordinatorLayout
@@ -52,6 +53,10 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         setContentView(R.layout.activity_main)
         ButterKnife.bind(this)
         setSupportActionBar(bar)
+        val params: (ViewGroup.MarginLayoutParams) =
+            boundFab.getLayoutParams() as ViewGroup.MarginLayoutParams
+        params.bottomMargin =
+            AppUtil.getActionBarHeight(this) + AppUtil.dpToPx(5, resources)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
@@ -59,6 +64,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
         setupBottomDrawer()
         apiService = ApiClient.getClient()!!.create(ApiService::class.java)
+
     }
 
     private fun setupBottomDrawer() {
