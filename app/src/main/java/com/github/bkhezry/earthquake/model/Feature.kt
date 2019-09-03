d265e3a843365d5c4f57c0d2be4a477b689c3127
Behrouz <bkhezry@gmail.com>
2019-09-03 15:41:08 +0430

    Extend Feature class from AbstractItem class. - implement methods for handle ViewHolder of feature items.

--- app/src/main/java/com/github/bkhezry/earthquake/model/Feature.kt
+++ app/src/main/java/com/github/bkhezry/earthquake/model/Feature.kt
@@ -1,20 +1,36 @@
 package com.github.bkhezry.earthquake.model
 
 
+import android.view.View
+import com.github.bkhezry.earthquake.R
 import com.google.android.gms.maps.model.LatLng
 import com.google.gson.annotations.SerializedName
 import com.google.maps.android.clustering.ClusterItem
+import com.mikepenz.fastadapter.FastAdapter
+import com.mikepenz.fastadapter.items.AbstractItem
 
 data class Feature(
     @SerializedName("type")
-    val type: String,
+    val typeString: String,
     @SerializedName("properties")
     val properties: Properties,
     @SerializedName("geometry")
     val geometry: Geometry,
     @SerializedName("id")
     val id: String
-) : ClusterItem {
+) : ClusterItem, AbstractItem<Feature.ViewHolder>() {
+    override fun getViewHolder(v: View): ViewHolder {
+        return ViewHolder(v)
+    }
+
+    override val type: Int
+        get() = R.id.fastadapter_sample_item_id
+
+    /** defines the layout which will be used for this item in the list  */
+    override val layoutRes: Int
+        get() = R.layout.feature_item
+
+
     override fun getSnippet(): String {
         return ""
     }
@@ -26,4 +42,17 @@ data class Feature(
     override fun getPosition(): LatLng {
         return LatLng(geometry.coordinates[1], geometry.coordinates[1])
     }
+
+    class ViewHolder(view: View) : FastAdapter.ViewHolder<Feature>(view) {
+
+
+        override fun bindView(item: Feature, payloads: MutableList<Any>) {
+
+        }
+
+        override fun unbindView(item: Feature) {
+
+        }
+    }
+
 }
