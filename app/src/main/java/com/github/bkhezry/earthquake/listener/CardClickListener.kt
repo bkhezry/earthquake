2294e5b99affec4e06d482cc60bbe87b5361ca35
Behrouz <bkhezry@gmail.com>
2019-09-07 15:28:35 +0430

    Add CardClickListener interface - handle click on card items and return position with data of clicked item

---
+++ app/src/main/java/com/github/bkhezry/earthquake/listener/CardClickListener.kt
@@ -0,0 +1,7 @@
+package com.github.bkhezry.earthquake.listener
+
+import com.github.bkhezry.earthquake.model.Feature
+
+interface CardClickListener {
+    fun selected(feature: Feature, position: Int)
+}
