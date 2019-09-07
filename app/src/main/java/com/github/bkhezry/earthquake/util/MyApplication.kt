d81c93d5166a6c772b0b3aa6ebb289acf673e0e0
Behrouz <bkhezry@gmail.com>
2019-09-07 17:49:49 +0430

    Add MyApplication class - access to application context static value.

---
+++ app/src/main/java/com/github/bkhezry/earthquake/util/MyApplication.kt
@@ -0,0 +1,24 @@
+package com.github.bkhezry.earthquake.util
+
+import android.app.Application
+import android.content.Context
+
+class MyApplication : Application() {
+
+    init {
+        instance = this
+    }
+
+    companion object {
+        private var instance: MyApplication? = null
+
+        fun applicationContext(): Context {
+            return instance!!.applicationContext
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        val context: Context = applicationContext()
+    }
+}
