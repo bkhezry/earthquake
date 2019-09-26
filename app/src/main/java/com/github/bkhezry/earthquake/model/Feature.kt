3f5846f03222a76b8bf3835422ed75e8a8436591
Behrouz <bkhezry@gmail.com>
2019-09-26 03:49:38 +0330

    change code style of feature class. change in date time showing data

--- app/src/main/java/com/github/bkhezry/earthquake/model/Feature.kt
+++ app/src/main/java/com/github/bkhezry/earthquake/model/Feature.kt
@@ -25,113 +25,113 @@ import java.util.*
 
 @Parcelize
 data class Feature(
-    @SerializedName("type")
-    val typeString: String,
-    @SerializedName("properties")
-    val properties: Properties,
-    @SerializedName("geometry")
-    val geometry: Geometry,
-    @SerializedName("id")
-    val id: String
+  @SerializedName("type")
+  val typeString: String,
+  @SerializedName("properties")
+  val properties: Properties,
+  @SerializedName("geometry")
+  val geometry: Geometry,
+  @SerializedName("id")
+  val id: String
 ) : ClusterItem, AbstractItem<Feature.ViewHolder>(), Parcelable {
-    override fun getViewHolder(v: View): ViewHolder {
-        return ViewHolder(v)
+  override fun getViewHolder(v: View): ViewHolder {
+    return ViewHolder(v)
+  }
+
+  override val type: Int
+    get() = R.id.fastadapter_sample_item_id
+
+  /** defines the layout which will be used for this item in the list  */
+  override val layoutRes: Int
+    get() = R.layout.feature_item
+
+
+  override fun getSnippet(): String {
+    return ""
+  }
+
+  override fun getTitle(): String {
+    return ""
+  }
+
+  override fun getPosition(): LatLng {
+    return LatLng(geometry.coordinates[1], geometry.coordinates[0])
+  }
+
+  class ViewHolder(view: View) : FastAdapter.ViewHolder<Feature>(view) {
+    val view: MaterialCardView = view as MaterialCardView
+    var infoButton: ImageButton = view.findViewById(R.id.info_button)
+    var title: TextView = view.findViewById(R.id.title_text_view)
+    var mag: TextView = view.findViewById(R.id.mag_text_view)
+    var date: TextView = view.findViewById(R.id.date_text_view)
+    override fun bindView(item: Feature, payloads: MutableList<Any>) {
+      view.clearAnimation()
+      if (item.isSelected) {
+        val color: Int = Color.argb(255, 255, 255, 51)
+        view.strokeColor = color
+        view.strokeWidth = AppUtil.dpToPx(1f)
+        view.foreground = FastAdapterUIUtils.getSelectablePressedBackground(
+          view.context,
+          FastAdapterUIUtils.adjustAlpha(color, 20),
+          50,
+          true
+        )
+      }
+      val calendar = Calendar.getInstance()
+      calendar.timeInMillis = item.properties.time
+      val format = SimpleDateFormat("E, MMM d, yyyy  hh:mm", Locale.getDefault())
+      val stringArray = item.properties.place.split("of")
+      title.text = stringArray[1]
+      mag.text = view.context.getString(R.string.mag_label)
+        .plus(
+          String.format(
+            "%.1f",
+            item.properties.mag
+          )
+        )
+      date.text = format.format(calendar.time)
+
     }
 
-    override val type: Int
-        get() = R.id.fastadapter_sample_item_id
+    override fun unbindView(item: Feature) {
+      title.text = null
+      mag.text = null
+      date.text = null
+      view.strokeColor = Color.WHITE
+      view.strokeWidth = AppUtil.dpToPx(0f)
+    }
+  }
 
-    /** defines the layout which will be used for this item in the list  */
-    override val layoutRes: Int
-        get() = R.layout.feature_item
-
-
-    override fun getSnippet(): String {
-        return ""
+  class InfoFabClickEvent(cardClickListener: CardClickListener) : ClickEventHook<Feature>() {
+    private var listener: CardClickListener = cardClickListener
+    override fun onBind(viewHolder: RecyclerView.ViewHolder): View? {
+      return if (viewHolder is ViewHolder) {
+        viewHolder.infoButton
+      } else null
     }
 
-    override fun getTitle(): String {
-        return ""
-    }
-
-    override fun getPosition(): LatLng {
-        return LatLng(geometry.coordinates[1], geometry.coordinates[0])
-    }
-
-    class ViewHolder(view: View) : FastAdapter.ViewHolder<Feature>(view) {
-        val view: MaterialCardView = view as MaterialCardView
-        var infoButton: ImageButton = view.findViewById(R.id.info_button)
-        var title: TextView = view.findViewById(R.id.title_text_view)
-        var mag: TextView = view.findViewById(R.id.mag_text_view)
-        var date: TextView = view.findViewById(R.id.date_text_view)
-        override fun bindView(item: Feature, payloads: MutableList<Any>) {
-            view.clearAnimation()
-            if (item.isSelected) {
-                val color: Int = Color.argb(255, 255, 255, 51)
-                view.strokeColor = color
-                view.strokeWidth = AppUtil.dpToPx(1f)
-                view.foreground = FastAdapterUIUtils.getSelectablePressedBackground(
-                    view.context,
-                    FastAdapterUIUtils.adjustAlpha(color, 20),
-                    50,
-                    true
-                )
-            }
-            val calendar = Calendar.getInstance()
-            calendar.timeInMillis = item.properties.time
-            val format = SimpleDateFormat("E, MMMM d, yyyy 'at' h:mm", Locale.getDefault())
-            val stringArray = item.properties.title.split("-")
-            title.text = stringArray[1].trim()
-            mag.text = view.context.getString(R.string.mag_label)
-                .plus(
-                    String.format(
-                        "%.1f",
-                        item.properties.mag
-                    )
-                )
-            date.text = format.format(calendar.time)
-
+    override fun onClick(
+      v: View,
+      position: Int,
+      fastAdapter: FastAdapter<Feature>,
+      item: Feature
+    ) {
+      if (!item.isSelected) {
+        val selectExtension: SelectExtension<Feature> = fastAdapter.requireExtension()
+        val selections = selectExtension.selections
+        if (selections.isNotEmpty()) {
+          for (selectedPosition in selections.iterator()) {
+            selectExtension.deselect(selectedPosition)
+            fastAdapter.notifyAdapterItemChanged(selectedPosition)
+          }
         }
+        selectExtension.select(position)
+        fastAdapter.notifyAdapterItemChanged(position)
 
-        override fun unbindView(item: Feature) {
-            title.text = null
-            mag.text = null
-            date.text = null
-            view.strokeColor = Color.WHITE
-            view.strokeWidth = AppUtil.dpToPx(0f)
-        }
+      }
+      AppUtil.vibrate()
+      listener.selected(item, position)
     }
-
-    class InfoFabClickEvent(cardClickListener: CardClickListener) : ClickEventHook<Feature>() {
-        private var listener: CardClickListener = cardClickListener
-        override fun onBind(viewHolder: RecyclerView.ViewHolder): View? {
-            return if (viewHolder is ViewHolder) {
-                viewHolder.infoButton
-            } else null
-        }
-
-        override fun onClick(
-            v: View,
-            position: Int,
-            fastAdapter: FastAdapter<Feature>,
-            item: Feature
-        ) {
-            if (!item.isSelected) {
-                val selectExtension: SelectExtension<Feature> = fastAdapter.requireExtension()
-                val selections = selectExtension.selections
-                if (selections.isNotEmpty()) {
-                    for (selectedPosition in selections.iterator()) {
-                        selectExtension.deselect(selectedPosition)
-                        fastAdapter.notifyAdapterItemChanged(selectedPosition)
-                    }
-                }
-                selectExtension.select(position)
-                fastAdapter.notifyAdapterItemChanged(position)
-
-            }
-            AppUtil.vibrate()
-            listener.selected(item, position)
-        }
-    }
+  }
 
 }
