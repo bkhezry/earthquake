38bcf60984384637dffc8d18d02f0c82e508a597
Behrouz <bkhezry@gmail.com>
2019-09-28 21:06:19 +0330

    change api function. add parameter for end point.

--- app/src/main/java/com/github/bkhezry/earthquake/service/ApiService.kt
+++ app/src/main/java/com/github/bkhezry/earthquake/service/ApiService.kt
@@ -3,9 +3,10 @@ package com.github.bkhezry.earthquake.service
 import com.github.bkhezry.earthquake.model.EarthquakeResponse
 import io.reactivex.Single
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ApiService {
 
-  @GET("all_hour.geojson")
-  fun getEarthquakes(): Single<EarthquakeResponse>
+  @GET("{endpoint}")
+  fun getEarthquakes(@Path("endpoint") get: String): Single<EarthquakeResponse>
 }
