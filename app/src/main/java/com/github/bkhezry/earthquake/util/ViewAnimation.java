fec5051edf07fd120a0880906078c90c85a27c66
Behrouz <bkhezry@gmail.com>
2019-10-03 01:00:44 +0330

    fix bug in expand animation function. change UI of about bottom sheet

--- app/src/main/java/com/github/bkhezry/earthquake/util/ViewAnimation.java
+++ app/src/main/java/com/github/bkhezry/earthquake/util/ViewAnimation.java
@@ -8,15 +8,13 @@ import android.view.animation.Transformation;
 public class ViewAnimation {
 
   public static void expand(final View v) {
-    Animation a = expandAction(v);
-    v.startAnimation(a);
-  }
-
-  private static Animation expandAction(final View v) {
-    v.measure(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+    int matchParentMeasureSpec = View.MeasureSpec.makeMeasureSpec(((View) v.getParent()).getWidth(), View.MeasureSpec.EXACTLY);
+    int wrapContentMeasureSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+    v.measure(matchParentMeasureSpec, wrapContentMeasureSpec);
     final int targetHeight = v.getMeasuredHeight();
 
-    v.getLayoutParams().height = 0;
+    // Older versions of android (pre API 21) cancel animations for views with a height of 0.
+    v.getLayoutParams().height = 1;
     v.setVisibility(View.VISIBLE);
     Animation a = new Animation() {
       @Override
@@ -33,9 +31,9 @@ public class ViewAnimation {
       }
     };
 
+    // Expansion speed of 1dp/ms
     a.setDuration((int) (targetHeight / v.getContext().getResources().getDisplayMetrics().density));
     v.startAnimation(a);
-    return a;
   }
 
   public static void collapse(final View v) {
