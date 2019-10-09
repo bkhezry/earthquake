a485236294df65002fab2787aa11bb8ffd21ba40
Behrouz <bkhezry@gmail.com>
2019-10-10 02:57:42 +0330

    add Kdoc to the source codes

--- app/src/main/java/com/github/bkhezry/earthquake/listener/CardClickListener.kt
+++ app/src/main/java/com/github/bkhezry/earthquake/listener/CardClickListener.kt
@@ -3,5 +3,11 @@ package com.github.bkhezry.earthquake.listener
 import com.github.bkhezry.earthquake.model.Feature
 
 interface CardClickListener {
+  /**
+   * Return selected feature & position of it
+   *
+   * @param feature Feature the instance of feature
+   * @param position Int the position ot feature in recycler view
+   */
   fun selected(feature: Feature, position: Int)
 }
